064ff5f4d7fbb25e1fe600c6349a159892ddbe1b
Manuel Palmieri <manuelpalm2002@gmail.com>
2024-12-11 16:39:16 +0100

    creato carrello e bottone del carrello

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 const url = "https://striveschool-api.herokuapp.com/books";
 const rowDiv = document.getElementById("rowDiv");
-let booksArray
+const cartOl = document.getElementById("cartOl");
+let booksArray;
 
 const retrieveData = () => {
     fetch(url)
@@ -28,17 +29,36 @@ function cards() {
          <div class="card-body">
          <h5 class="card-title">${booksArray[i].title}</h5>
          <p class="card-text">${booksArray[i].category}</p>
-         <p class="card-text">${booksArray[i].price}</p>
-         <a href="javascript:void(0)" class="btn btn-primary">SCARTA</a>
+         <p class="card-text">${booksArray[i].price}€</p>
+         <a href="javascript:void(0)" class="btn btn-primary compra">COMPRA</a>
+         <a href="javascript:void(0)" class="btn btn-primary scarta">SCARTA</a>
          </div>
          </div>`;
          
     };
-    const scarta = document.querySelectorAll("a");
+    const scarta = document.querySelectorAll(".scarta");
     for (let i=0; i< booksArray.length; i++){
          scarta[i].addEventListener("click", function(){
             booksArray.splice(i, 1);
             cards()
          });
     };
+    cartfunc()
+}
+
+function cartfunc() {
+    const compra = document.querySelectorAll(".compra");
+    for (let i=0; i< booksArray.length; i++){
+        compra[i].addEventListener("click", function(){
+            const li = document.createElement("li");
+            li.innerHTML = `${booksArray[i].title} ${booksArray[i].price}€`;
+            cartOl.appendChild(li);
+            const cartBtn = document.createElement("button");
+            cartBtn.innerHTML= "ciao";
+            li.appendChild(cartBtn);
+            cartBtn.addEventListener("click", function(){
+                li.style.display = "none";
+            });
+        })
+    }
 }
